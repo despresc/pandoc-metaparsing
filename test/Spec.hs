22350b11edbea11588c7d58a7125dfe033112b7f
Christian Despres <christian.j.j.despres@gmail.com>
2019-04-30 23:43:29 -0400

    Add another test

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,6 +11,7 @@ resultExplode (Error e)   = error $ simpleErrorShow e
 resultExplode (Success a) = pure a
 
 -- Make sure that the example used in the README and the haddock introduction works.
+-- (The onlyFields part was added for another simple test).
 
 data ContributorTitle = NoTitle | HasTitle Inlines
   deriving (Eq, Ord, Show)
@@ -27,7 +28,7 @@ instance FromValue ContributorTitle where
   parseValue = symbol "no-title" NoTitle <|> HasTitle <$> parseValue <?> "inline title"
 
 instance FromValue Contributor where
-  parseValue = object $ Contributor <$> field "name" <*> "location" .! parseInlines <*> "title" .?! NoTitle
+  parseValue = object $ onlyFields ["name", "location", "title"] $ Contributor <$> field "name" <*> "location" .! parseInlines <*> "title" .?! NoTitle
 
 main :: IO ()
 main = hspec $ do
@@ -44,8 +45,11 @@ main = hspec $ do
                                             , toSource nameW locationW (Just titleW) ] nullMeta
         invalidTitle1 = MetaString "foo"
         invalidMeta = setMeta "contributors" [ toSource (toMetaValue nameS) (toMetaValue locationS) (Just invalidTitle1)] nullMeta
+        invalidMeta' = setMeta "contributors" [ insert "foo" (str "thing") $ toSource nameS locationS Nothing ] nullMeta
         expectedError = MetaWhenParseError "field contributors" . MetaWhenParseError "field title" . MetaExpectGotError (expectationFromList [ "no-title", "inline title"])
     it "parses valid contributors" $
       fromMetaField "contributors" sourceMeta `shouldBe` Success [expectS, expectW]
     it "throws correctly on one invalid title field" $
       (fromMetaField "contributors" invalidMeta :: Result Contributors) `shouldBe` Error (expectedError (Just "foo"))
+    it "throws correctly if there are unxpected fields" $
+      (fromMetaField "contributors" invalidMeta' :: Result Contributors) `shouldBe` Error (MetaWhenParseError "field contributors" $ MetaFieldUnknown "foo")
